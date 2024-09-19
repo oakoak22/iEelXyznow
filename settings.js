df14e6d401ac4e7230b800c13cc3c21666d1bdff
oakoak22 <nudanudaser@gmail.com>
2024-09-19 15:03:43 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'Actrotophiles', // Ganti dengan nama owner lu
+  botToken: '7708991311:AAEqxf5CZqB3Uc6nbBpM-anGER4bettJYIo', // Ganti dengan token bot Telegram lu
+  owner: '6576625630', //OWNER user id
+};
+
+module.exports = settings;
